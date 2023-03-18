e21734c9b0a1052bc41c21c62b0479558ed7c797
arduan <ivanov.arduan@yandex.ru>
2023-03-18 10:05:41 +0300

    Добавил модуль django-tastypie для API. Создал resouces.py. Прописал urls для resources.py

--- intensiv/urls.py
+++ intensiv/urls.py
@@ -1,11 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from .views import VitalSignsListView, VitalSignsCreateView, PatientListView, PatientDetailView
-
+from .resources import PatientResource
 app_name = 'patients'
+patient_resource = PatientResource()
 
 urlpatterns = [
     path('', PatientListView.as_view(), name='patient_list'),
     path('vital/', VitalSignsListView.as_view(), name='vital_signs_list'),
     path('<int:pk>/', PatientDetailView.as_view(), name='patient_detail'),
     path('add/', VitalSignsCreateView.as_view(), name='vital_signs_create'),
+    path('api/', include(patient_resource.urls)),
 ]
